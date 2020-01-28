cc892ab2ed4beb3a9d0044fb4c28536c7f3bd50d
Edward Stoner <edwardlstoner@gmail.com>
2020-01-28 08:26:42 -0700

    Finish tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -23,5 +23,13 @@ describe('Deck', function() {
     expect(deck.cards).to.equal([card1, card2, card3]);
   });
 
+  it.skip('should be able to count cards in deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(countCards()).to.equal(3);
+  });
+
 
 });
