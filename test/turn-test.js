67addb8f58c0a8639f443ef68b67456cd5ee891f
Edward Stoner <edwardlstoner@gmail.com>
2020-01-27 19:48:57 -0700

    Finish turn tests

--- test/turn-test.js
+++ test/turn-test.js
@@ -17,14 +17,38 @@ describe('Turn', function() {
 
   it('should store a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
-    expect(turn.guess).to.equal('object');
+    const turn = new Turn('array', card);
+    expect(turn.guess).to.equal('array');
   });
 
   it('should store a card', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
+    const turn = new Turn('array', card);
     expect(turn.card).to.equal(card);
   });
+
+  it('should be able to return the guess'), function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.returnGuess().to.equal('array'))
+  }
+
+  it('should be able to return the card'), function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.returnCard().to.equal(card))
+  }
+
+  it('should be able to evaluate a guess'), function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.evaluateGuess().to.equal(false))
+  }
+
+  it('should be able to give feedback'), function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.giveFeedback().to.equal('incorrect!'))
+  }
 
 });
