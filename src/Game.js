53997432d41b6cab0d065ac2c81cb2fe17725304
Edward Stoner <edwardlstoner@gmail.com>
2020-01-29 08:09:41 -0700

    Add method and properties for Game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
+const Round = require('../src/Round')
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +17,19 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    this.currentRound++;
+    var cards = [];
+    prototypeQuestions.forEach(question => {
+      var card = new Card(question.id, question.question, question.answers, question.correctAnswer)
+      cards.push(card)
+    })
+    const deck = new Deck(cards)
+    const round = new Round(deck)
+    this.printMessage(deck, round)
+    this.printQuestion(round)
   }
 }
 
