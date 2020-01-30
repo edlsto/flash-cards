c8919ca2fa09bb7c5a7803fb47fd231d06c896d8
Edward Stoner <edwardlstoner@gmail.com>
2020-01-29 17:47:57 -0700

    Add additional arguments for game.start

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const Game = require('./src/Game')
 const data = require('./src/data');
 const prototypeQuestions = data.prototypeData;
 
-
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
 const game = new Game();
-game.start(prototypeQuestions);
+game.start(prototypeQuestions, game, 'original');
