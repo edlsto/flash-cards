e0716a57e9d1f9f6e12d3db77d9610b6db6da68b
Edward Stoner <edwardlstoner@gmail.com>
2020-01-29 17:51:40 -0700

    Add parameters, logic for message

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
+const secondSet = data.secondSet;
 const util = require('./util');
 const Card = require('../src/Card')
 const Deck = require('../src/Deck')
@@ -14,22 +15,37 @@ class Game {
 -----------------------------------------------------------------------`)
   }
 
-  printQuestion(round) {
-      util.main(round);
+  printMessageRound2(deck, round) {
+      console.log(`Let's try another round! You are playing with ${deck.countCards()} cards.
+-----------------------------------------------------------------------`)
   }
 
-  start(questions) {
+  printQuestion(round, game) {
+      util.main(round, game);
+  }
+
+  start(questions, game, deckName) {
+    //round1 - OG cards
     var cards = [];
     questions.forEach(question => {
       var card = new Card(question.id, question.question, question.answers, question.correctAnswer)
       cards.push(card)
     })
-    const deck = new Deck(cards)
+    const deck = new Deck(cards, deckName)
     const round = new Round(deck)
-    this.printMessage(deck, round)
-    this.printQuestion(round)
     this.currentRound = round;
+    if (game.currentRound.deck.name === 'original') {
+      this.printMessage(deck, round)
+    } else {
+      this.printMessageRound2(deck, round)
+    }
+    this.printQuestion(round, game)
+
+    //round 2 - new dataset
   }
+
+
+
 }
 
 module.exports = Game;
