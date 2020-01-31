789d0c4371647833041b773c2e59bbc928647234
Edward Stoner <edwardlstoner@gmail.com>
2020-01-31 07:55:09 -0700

    Refactor util

--- src/util.js
+++ src/util.js
@@ -1,7 +1,4 @@
 const inquirer = require('inquirer');
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
-const secondSet = data.secondSet;
 
 const genList = (round) => {
   let card = round.returnCurrentCard();
@@ -32,24 +29,75 @@ const confirmUpdate = (id, round) => {
   }
 }
 
-async function main(round, game) {
+async function main(round) {
+
   const currentRound = await getRound(round);
   const getAnswer = await inquirer.prompt(genList(currentRound));
   const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round));
-  if (!round.returnCurrentCard() && round.calculatePercentCorrect() < 90 & game.currentRound.deck.name.includes('review') === false) {
-    round.endRound();
-    console.log('Your score was too low! Try this round again!')
-    game.currentRound.deck.name === 'original' ? game.start(prototypeQuestions, game, game.currentRound.deck.name) : game.start(secondSet, game, game.currentRound.deck.name)
-  } else if (!round.returnCurrentCard() && !game.currentRound.deck.name.includes('review') && round.calculatePercentCorrect() < 100) {
-    round.endRound();
-    game.review(round);
-  } else if (!round.returnCurrentCard() && game.currentRound.deck.name === 'original-review') {
-    game.start(secondSet, game, 'second-set');
-  } else if (!round.returnCurrentCard())  {
-    round.endRound();
-  } else {
-    main(round, game);
-  }
+
+    if(!round.returnCurrentCard()) {
+      round.endRound();
+    } else {
+      main(round);
+    }
 }
 
 module.exports.main = main;
+
+
+
+// const inquirer = require('inquirer');
+// const data = require('./data');
+// const prototypeQuestions = data.prototypeData;
+// const secondSet = data.secondSet;
+//
+// const genList = (round) => {
+//   let card = round.returnCurrentCard();
+//
+//   let choices = card.answers.map((answer, index) => {
+//     return {
+//       key: index,
+//       value: answer
+//     }
+//   });
+//   return {
+//     type: 'rawlist',
+//     message: card.question,
+//     name: 'answers',
+//     choices: choices
+//   };
+// }
+//
+// const getRound = (round) => {
+//   return Promise.resolve(round);
+// }
+//
+// const confirmUpdate = (id, round) => {
+//   const feedback = round.takeTurn(id);
+//   return {
+//     name: 'feedback',
+//     message: `Your answer of ${id} is ${feedback}`
+//   }
+// }
+//
+// async function main(round, game) {
+//   const currentRound = await getRound(round);
+//   const getAnswer = await inquirer.prompt(genList(currentRound));
+//   const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round));
+//   if (!round.returnCurrentCard() && round.calculatePercentCorrect() < 90 & game.currentRound.deck.name.includes('review') === false) {
+//     round.endRound();
+//     console.log('Your score was too low! Try this round again!')
+//     game.currentRound.deck.name === 'original' ? game.start(prototypeQuestions, game, game.currentRound.deck.name) : game.start(secondSet, game, game.currentRound.deck.name)
+//   } else if (!round.returnCurrentCard() && !game.currentRound.deck.name.includes('review') && round.calculatePercentCorrect() < 100) {
+//     round.endRound();
+//     game.review(round);
+//   } else if (!round.returnCurrentCard() && game.currentRound.deck.name === 'original-review') {
+//     game.start(secondSet, game, 'second-set');
+//   } else if (!round.returnCurrentCard())  {
+//     round.endRound();
+//   } else {
+//     main(round, game);
+//   }
+// }
+//
+// module.exports.main = main;
