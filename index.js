07d9f63442a4d603ead8de04df85a58179138eb6
Edward Stoner <edwardlstoner@gmail.com>
2020-01-31 07:53:27 -0700

    Refactor util file

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let app = http.createServer();
 const Game = require('./src/Game')
 const data = require('./src/data');
 const prototypeQuestions = data.prototypeData;
+const secondSet = data.secondSet;
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
-const game = new Game();
-game.start(prototypeQuestions, game, 'original');
+const game = new Game([prototypeQuestions]);
+game.start();
