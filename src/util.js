fe1ab89a02c7441a6ee6124481ea59dc5452f0d9
Edward Stoner <edwardlstoner@gmail.com>
2020-01-30 17:00:25 -0700

    Combine console log functions

--- src/util.js
+++ src/util.js
@@ -37,18 +37,15 @@ async function main(round, game) {
   const getAnswer = await inquirer.prompt(genList(currentRound));
   const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round));
   if (!round.returnCurrentCard() && round.calculatePercentCorrect() < 80 & game.currentRound.deck.name.includes('review') === false) {
-    round.giveTime();
     round.endRound();
     console.log('Your score was too low! Try this round again!')
     game.currentRound.deck.name === 'original' ? game.start(prototypeQuestions, game, game.currentRound.deck.name) : game.start(secondSet, game, game.currentRound.deck.name)
   } else if (!round.returnCurrentCard() && !game.currentRound.deck.name.includes('review') && round.calculatePercentCorrect() < 100) {
-    round.giveTime();
     round.endRound();
     game.review(round);
   } else if (!round.returnCurrentCard() && game.currentRound.deck.name === 'original-review') {
     game.start(secondSet, game, 'second-set');
   } else if (!round.returnCurrentCard())  {
-    round.giveTime()
     round.endRound();
   } else {
     main(round, game);
